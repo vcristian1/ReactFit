7c35f202f97e3719e32698253a68253506417f1b
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-26 17:31:56 -0500

    imported image1-image6 from assets into the index.tsx within ourclasses directory

--- client/src/scenes/ourclasses/index.tsx
+++ client/src/scenes/ourclasses/index.tsx
@@ -1,4 +1,10 @@
 import { SelectedPage } from "../../shared/types";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
+import image3 from "../../assets/image3.png";
+import image4 from "../../assets/image4.png";
+import image5 from "../../assets/image5.png";
+import image6 from "../../assets/image6.png";
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
