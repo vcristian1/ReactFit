d4d5e3bf4626741571e121af8c816d3f8f2aee67
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-26 13:18:54 -0500

    created reuasble h1 component in shared and imported it in benefits/index.tsx

--- client/src/scenes/benefits/index.tsx
+++ client/src/scenes/benefits/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { SelectedPage } from '../../shared/types';
+import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
+import { motion } from 'framer-motion';
+import HText from '../../shared/HText';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -7,7 +10,19 @@ type Props = {
 
 const Benefits = ({ setSelectedPage }: Props) => {
   return (
-    <div>Benefits</div>
+    <section
+     id='benefits'
+     className='mx-auto min-h-full w-5/6 py-20'
+    >
+        <motion.div 
+         className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+        >
+            <div>
+                <HText>MORE THAN JUST A GYM</HText>
+            </div>
+        </motion.div>
+    </section>
   )
 }
 
