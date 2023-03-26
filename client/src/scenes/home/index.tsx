4f2a9bd906d3ba8462a722a73d185cf05d1b5e1e
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-26 12:26:01 -0500

    added tailwind classes to the three containers holding the images for the sponsors so that the container is only 150 px high has a pink background, and the images are centered with an even gap in between

--- client/src/scenes/home/index.tsx
+++ client/src/scenes/home/index.tsx
@@ -61,9 +61,9 @@ const Home = ({ setSelectedPage }: Props) => {
 
         {/* Sponsors Here */}
         {isAboveMediumScreens && (
-            <div>
-                <div>
-                    <div>
+            <div className="h-[150px] w-full bg-primary-100 py-10">
+                <div className="mx-auto w-5/6">
+                    <div className="flex w-3/5 items-center justify-between gap-8">
                         <img src={SponsorRedbull} alt="sponsor-redbull" />
                         <img src={SponsorFortune} alt="sponsor-fortune" />
                         <img src={SponsorForbes} alt="sponsor-forbes" />
