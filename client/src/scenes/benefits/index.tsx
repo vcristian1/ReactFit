11675ca577bd061a19bc6f5f9a6e8696a53192f5
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-26 14:01:41 -0500

    created childVariant object in Benefit.tsx to stagger the animation of the benefits, and set the variants property in the motion.div within Benefit.tsxto do so

--- client/src/scenes/benefits/index.tsx
+++ client/src/scenes/benefits/index.tsx
@@ -70,10 +70,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
              initial="hidden"
              whileInView="visible"
              viewport={{ once: true, amount: 0.5 }}
-             variants={{
-                hidden: { opacity: 0, x: -50 },
-                visible: { opacity: 1, x: 0 },
-              }}             
+             variants={container}             
             >
                 {benefits.map((benefit: BenefitType) => (
                     <Benefit 
