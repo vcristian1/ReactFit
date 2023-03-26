7559a9216059d239370b5723b1ba555c1667bb4c
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-26 14:40:32 -0500

    added p tags containing the description within its container

--- client/src/scenes/benefits/index.tsx
+++ client/src/scenes/benefits/index.tsx
@@ -100,8 +100,23 @@ const Benefits = ({ setSelectedPage }: Props) => {
                         </div>
                     </div>
 
-                    {/* DESCRIPTION HERE */}
-
+                    {/* DESCRIPTION PT2 HERE */}
+                    <div>
+                        <p className="my-5">
+                            Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                            egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                            fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                            nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                            tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                            Felis orci diam odio.
+                        </p>
+                        <p className="mb-5">
+                            Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                            tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                            facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                            enim mattis odio in risus nunc.
+                        </p>
+                    </div>
                     {/* BUTTON HERE */}
 
                 </div>                
