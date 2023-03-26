7cee1e15c5c5e2f61fb62931ccd1b4815cf5871f
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-26 08:41:33 -0500

    used custom hook for media queries and set  min-width-1060px media query equal to isAboveMediumScreens, and used ternary operator to write logic for what will render if the screen is above medium screens, and what will render if it is not which is mobile

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "../../shared/types";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -11,6 +12,8 @@ type Props = {
 
 const Navbar = ({  selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
 
   return (
     <nav>
@@ -23,20 +26,28 @@ const Navbar = ({  selectedPage, setSelectedPage }: Props) => {
                 {/* Left Side */}
                 <img src={Logo} alt="logo" />
                 {/* Right Side */}
-                <div className={`${flexBetween} w-full`}>
-                    {/* Inner Left Side (All Links) */}
-                    <div className={`${flexBetween} gap-8 text-sm`}>
-                        <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
-                        <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+
+                {isAboveMediumScreens ? (
+                    <div className={`${flexBetween} w-full`}>
+                        {/* Inner Left Side (All Links) */}
+                        <div className={`${flexBetween} gap-8 text-sm`}>
+                            <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                        </div>
+                        {/* Inner Right Side (Sign In and Become a Member buttons)*/}
+                        <div className={`${flexBetween} gap-2 text-sm`}>
+                            <button>Sign In</button>
+                            <button>Become a Member</button>
+                        </div>
                     </div>
-                    {/* Inner Right Side (Sign In and Become a Member buttons)*/}
-                    <div className={`${flexBetween} gap-2 text-sm`}>
-                        <button>Sign In</button>
-                        <button>Become a Member</button>
-                    </div>
-                </div>
+                    ) : (
+                        // If it is not aboveModeiumScreen then the below code will render for mobile screens
+                        <button className="rounded-full bg-secondary-500 p-2" onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                            <Bars3Icon className="h-6 w-6 text-white"/>
+                        </button>
+                    )}
             </div>
           </div>
         </div>
