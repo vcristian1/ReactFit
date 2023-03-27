05f9f04bab026821be42470ebb37fd7014004634
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-27 09:22:43 -0500

    created overlayStyles variable which holds a template string of tailwind classes applied to the container of the li so when you hover you see the text and description

--- client/src/scenes/ourclasses/Class.tsx
+++ client/src/scenes/ourclasses/Class.tsx
@@ -7,14 +7,15 @@ type Props = {
 }
 
 const Class = ({ name, description, image}: Props) => {
+  const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90`
   return (
-    <section>
-        <div>
-            {name}
-            {description}
-            {image}
+    <li className='relative mx-5 in-line-block h-[380px] w-[450px]'>
+        <div className={overlayStyles}>
+            <p>{name}</p>
+            <p>{description}</p>
         </div>
-    </section>
+        <img src={image} alt={`${image}`} />
+    </li>
   )
 }
 
