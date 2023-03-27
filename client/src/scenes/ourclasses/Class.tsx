f1ff4178077a0ba421d618172939b34dcba3a647
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-27 09:06:04 -0500

    the class component is rendering its content within the index.tsx page map function  in the ourclasses directory

--- client/src/scenes/ourclasses/Class.tsx
+++ client/src/scenes/ourclasses/Class.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 type Props = {
-    
+
 }
 
 const Class = (props: Props) => {
--- client/src/shared/types.ts
+++ client/src/shared/types.ts
@@ -5,13 +5,13 @@ export enum SelectedPage {
     ContactUs = "contactus"
   }
 
-  export interface BenefitType {
+export interface BenefitType {
     icon: JSX.Element;
     title: string;
     description: string;
   }
 
-  export interface ClassType {
+export interface ClassType {
     name: string;
     description?: string;
     image: string;
