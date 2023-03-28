e8adfe4958b136211a0af3062f72fce06a98522f
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-27 19:14:40 -0500

    added additional content to footer including importing the logo, and additional containers to hold the logo image, description, and copyright information

--- client/src/scenes/footer/index.tsx
+++ client/src/scenes/footer/index.tsx
@@ -1,9 +1,24 @@
+import Logo from "../../assets/logo.png";
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
+    <footer className="bg-primary-100 py-16">
+        <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+            <div className="mt-16 basis-1/2 md:mt-0">
+                <img src={Logo} alt="logo" />
+                <p className="my-5">
+                    Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
+                    purus et arcu massa dictum condimentum. Augue scelerisque iaculis
+                    orci ut habitant laoreet. Iaculis tristique.
+                </p>
+                <p>© Evogym All Rights Reserved.</p>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+    </footer>
   )
 }
 
