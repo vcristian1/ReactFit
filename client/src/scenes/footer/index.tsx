62500dd8935ef34d03b311d7bb06493f306adc01
Cristian Vargas <cristian.v0223@gmail.com>
2023-03-27 19:30:10 -0500

    oimported anchor link and added links to links section of the footer. Footer complete

--- client/src/scenes/footer/index.tsx
+++ client/src/scenes/footer/index.tsx
@@ -1,8 +1,12 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import Logo from "../../assets/logo.png";
+import { SelectedPage } from "../../shared/types";
 
-type Props = {}
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
 
-const Footer = (props: Props) => {
+const Footer = ({ setSelectedPage }: Props) => {
   return (
     <footer className="bg-primary-100 py-16">
         <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
@@ -15,8 +19,35 @@ const Footer = (props: Props) => {
                 </p>
                 <p>© Evogym All Rights Reserved.</p>
             </div>
-            <div></div>
-            <div></div>
+            <div className="mt-16 basis-1/4 md:mt-0">
+                <h4 className="font-bold">Links</h4>
+                <AnchorLink
+                    className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+                    onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                    href={`#${SelectedPage.Benefits}`}
+                >
+                    <p className="text-[16px] mt-2">Benefits</p>
+                </AnchorLink>
+                <AnchorLink
+                    className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+                    onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                    href={`#${SelectedPage.OurClasses}`}
+                >
+                    <p className="text-[16px] mt-2">Our Classes</p>
+                </AnchorLink>
+                <AnchorLink
+                    className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+                    onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                    href={`#${SelectedPage.ContactUs}`}
+                >
+                    <p className="text-[16px] mt-2">Learn More</p>
+                </AnchorLink>
+            </div>
+            <div className="mt-16 basis-1/4 md:mt-0">
+                <h4 className="font-bold">Contact Us</h4>
+                <p className="my-5">customerservice@evogym.com</p>
+                <p>+1 (333) 425 6825</p>
+            </div>
         </div>
     </footer>
   )
